1e9ab43799a3f6281934b70b981b7de9a414211a
Coltyn Beam <ralvius@gmail.com>
2022-01-23 18:10:03 -0500

    wxWidget Update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shiro/Documents/cppfiles/scratch
+# Build directory: /home/shiro/Documents/cppfiles/scratch/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
